16b0eab12dafd07afe79b2f38b48611b2a2473be
blahblah <HTCNik36@gmail.com>
2016-11-29 21:57:48 +0200

    add first step

--- index.php
+++ index.php
@@ -1,24 +1,13 @@
-<?php
-class Telegram {
-	const BOT_TOKEN = '275682784:AAGX9PhODUTbCJWh0stpVCMWFZooVGw5qlM';
-	const API_URL = 'https://api.telegram.org/bot'. self::BOT_TOKEN .'/';
-	const CHAT_ID = '280628855';
-
-	public $text = 'Ещё один тест!';
-
-	public function send($text) {
-		$parametrs = [
-					'chat_id' => self::CHAT_ID,
-					'text' => $this->text,
-		];
-
-		$url = ''. self::API_URL .'sendMessage?'. http_build_query($parametrs);
-
-		$grub = file_get_contents($url);
-
-		return $grub;
-	}
-}
-
-$bot = new Telegram();
-$bot->send();
+<html>
+<head>
+<title>Отправить сообщение себе в телеграм</title>
+</head>
+<body>
+	<h1>Форма отправки</h1>
+	<p><em>Здесь вы можете написать себе письмо и оно отправиться вам в Telegram</em></p>
+	<form action="telegram.php" method="POST">
+	<input type="text" name="msg">
+	<button type="submit">Send</button>
+	</form>
+</body>
+</html>
