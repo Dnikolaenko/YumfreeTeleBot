2d191e07cb1eca575938025d98a5425a300cbfca
Developer <HTCNik36@gmail.com>
2017-01-11 16:45:04 +0200

    add new methods and testing it(no webhooks way)

--- telegram.php
+++ telegram.php
@@ -1,19 +1,24 @@
 <?php
-class Telegram {
+
+class Telegram
+{
 	const BOT_TOKEN = '275682784:AAGX9PhODUTbCJWh0stpVCMWFZooVGw5qlM';
 	const API_URL = 'https://api.telegram.org/bot'. self::BOT_TOKEN .'/';
 	const CHAT_ID = '280628855';
 
-	public $text = "";
+	protected $text = "";
+	public $data;
 
-	public function __construct(){
+	public function __construct()
+	{
 		if(isset($_POST)){
 			$msg = $_POST['msg'];
 			$this->text = $msg;
 		}
 	}
 
-	public function send($text) {
+	public function send($text)
+	{
 		$parametrs = [
 					'chat_id' => self::CHAT_ID,
 					'text' => $this->text,
@@ -33,7 +38,46 @@ class Telegram {
 			return false;
 		}
 	}
+
+	public function getUpdates()
+	{
+		$url = self::API_URL . 'getUpdates';
+		$data = file_get_contents($url);
+		$all = json_decode($data, true);
+		$this->data = $all;
+		return $all;
+	}
+
+	public function answer()
+	{
+		$data = $this->data;
+		if(!$data == null) {
+				foreach($data as $val) {
+					foreach($val as $msg) {
+							foreach($msg as $pp) {
+								//var_dump($pp['text']);
+								if($pp['text'] == 'lol') {
+									$com = "Ты ввел первую команду!";
+									$parametrs = [
+												'chat_id' => self::CHAT_ID,
+												'text' => $com,
+									];
+									$url = self::API_URL . 'sendMessage?' . http_build_query($parametrs);
+									// var_dump($url);
+									$grub = file_get_contents($url);
+									return $grub;
+								}
+							}
+					}
+				}
+		} else {
+			echo "Возникла какая то проблема!";
+			return false;
+		}
+	}
 }
 
 $bot = new Telegram();
-$bot->send();
+//$bot->send();
+$bot->getUpdates();
+$bot->answer();
